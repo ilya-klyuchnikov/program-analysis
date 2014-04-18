09b021df8561d87b9a813a74ed238a86bb476c55
Ilya Klyuchnikov <ilya.klyuchnikov@gmail.com>
2014-04-18 09:33:23 +0400

    Very busy expressions analysis

---
+++ src/main/scala/pa/dataFlow/VeryBusyExpressions.scala
@@ -0,0 +1,49 @@
+package pa.dataFlow
+
+import pa.Analysis
+import pa.ast._
+import pa.controlFlow._
+
+case class VeryBusyExpressions(prog: Stmt) extends Analysis[Set[AExpr]] {
+  val points = programPoints(prog)
+  val eBlocks = blocks(prog)
+  val flows = flowGraph(prog).flows
+  val programAExprs = eBlocks.map(aExprs).reduce(_ ++ _)
+
+  override def equations(): List[Equation] = points.map {
+    case pp@Exit(l) if finalLabels(prog).contains(l) =>
+      Equation(
+        pp,
+        sol => Set()
+      )
+    case pp@Exit(l) =>
+      val tos = for {(`l`, to) <- flows} yield to
+      Equation(
+        pp,
+        sol => tos.map(l => sol(Entry(l))).reduce(_ & _)
+      )
+    case pp@Entry(l) =>
+      val block = eBlocks.find(_.label == l).get
+      Equation(
+        pp,
+        sol => sol(Exit(l)) -- kill(block) ++ gen(block)
+      )
+  }
+
+  override def zeroApproximation(): Solution =
+    points.map(_ -> programAExprs).toMap
+
+  def kill(block: ElementaryBlock): Set[AExpr] = block match {
+    case LabeledAssignment(x, _, _) =>
+      programAExprs.filter(vars(_)(x))
+    case _ => Set()
+  }
+
+  def gen(block: ElementaryBlock): Set[AExpr] = block match {
+    case LabeledAssignment(x, ae, _) =>
+      aExprs(ae)
+    case LabeledBExpr(be, _) =>
+      aExprs(be)
+    case _ => Set()
+  }
+}
---
+++ src/test/scala/VeryBusyExpressionsSpec.scala
@@ -0,0 +1,46 @@
+package pa.test
+
+import pa.ast._
+import pa.dataFlow.VeryBusyExpressions
+import pa.dsl._
+import pa.controlFlow._
+
+class VeryBusyExpressionsSpec extends org.scalatest.FunSpec {
+
+  describe("Very Busy Expressions Analysis") {
+    it("Example 2.9") {
+      val prog =
+        Program(
+          `if`('a > 'b |: 1)(
+            ('x := 'b - 'a) |: 2,
+            ('x := 'a - 'b) |: 3
+          ) (
+            ('y := 'b - 'a) |: 4,
+            ('x := 'a - 'b) |: 5
+          )
+        )
+
+      val actual = VeryBusyExpressions(prog).solve()
+      val expected = Map[ProgramPoint, Set[AExpr]](
+        Entry(1) -> Set('a - 'b, 'b - 'a),
+        Exit(1)  -> Set('a - 'b, 'b - 'a),
+        Entry(2) -> Set('a - 'b, 'b - 'a),
+        Exit(2)  -> Set('a - 'b),
+        Entry(3) -> Set('a - 'b),
+        Exit(3)  -> Set(),
+        Entry(4) -> Set('a - 'b, 'b - 'a),
+        Exit(4)  -> Set('a - 'b),
+        Entry(5) -> Set('a - 'b),
+        Exit(5)  -> Set()
+      )
+
+      val points = programPoints(prog)
+      for (p <- points) {
+        assert(p -> actual(p) === p -> expected(p))
+      }
+
+      assert(actual === expected)
+    }
+  }
+
+}
