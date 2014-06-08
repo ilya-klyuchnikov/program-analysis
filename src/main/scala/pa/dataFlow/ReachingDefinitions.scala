d4eb319e68c240f6fb1426bfebf83cc007774512
Ilya Klyuchnikov <ilya.klyuchnikov@gmail.com>
2014-06-08 14:17:45 +0400

    Reaching definitions in terms of kill/gen

--- src/main/scala/pa/dataFlow/ReachingDefinitions.scala
+++ src/main/scala/pa/dataFlow/ReachingDefinitions.scala
@@ -13,18 +13,11 @@ case class ReachingDefinitions(prog: Stmt) extends Analysis[Set[(Var, Label)]] {
 
   override def equations(): List[Equation] = points.map {
     case pp@Exit(l) =>
-      eBlocks.find(_.label == l).get match {
-        case LabeledAssignment(v, ae, _) =>
-          Equation(
-            pp,
-            sol => sol(Entry(l)) -- allLabels.map(v -> _) + (v -> l)
-          )
-        case _ =>
-          Equation(
-            pp,
-            sol => sol(Entry(l))
-          )
-      }
+      val block = eBlocks.find(_.label == l).get
+      Equation(
+        pp,
+        sol => sol(Entry(l)) -- kill(block) ++ gen(block)
+      )
     case pp@Entry(l) if l == initialLabel(prog) =>
       Equation(
         pp,
@@ -40,4 +33,17 @@ case class ReachingDefinitions(prog: Stmt) extends Analysis[Set[(Var, Label)]] {
 
   override def zeroApproximation(): Solution =
     points.map(_ -> Set[(Var, Label)]()).toMap
+
+  def kill(block: ElementaryBlock): Set[(Var, Label)] = block match {
+    case LabeledAssignment(v, ae, _) =>
+      allLabels.map(v -> _)
+    case _ =>
+      Set()
+  }
+
+  def gen(block: ElementaryBlock): Set[(Var, Label)] = block match {
+    case LabeledAssignment(v, _, l) =>
+      Set(v -> l)
+    case _ => Set()
+  }
 }
